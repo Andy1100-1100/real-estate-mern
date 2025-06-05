9ea5199b3f6807c81af72f5bffb56c6b22dcb4b3
Andy1100-1100 <andyansah11@gmail.com>
2025-06-05 14:19:16 +0000

    complete image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,8 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY
-  ,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-estate-2c09c.firebaseapp.com",
   projectId: "mern-estate-2c09c",
   storageBucket: "mern-estate-2c09c.firebasestorage.app",
