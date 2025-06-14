4c7dfdc6ca8ad94e6881524793be503c0513edd6
Andy1100-1100 <andyansah11@gmail.com>
2025-06-14 10:10:48 +0000

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Andy Estate</h1>
+      <p className='mb-4 text-slate-700'>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, 
+        making this the first true generator on the Internet. 
+        It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, 
+        to generate Lorem Ipsum which looks reasonable. 
+        The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+
+        <p className='mb-4 text-slate-700'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, 
+          making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+          looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+          and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          <p className='mb-4 text-slate-700'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+            as opposed to using 'Content here, content here', making it look like readable English.</p>
     </div>
   )
 }
